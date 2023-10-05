9eb975854729783644ebb4bcbe678be8404b41ae
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-10-05 17:34:41 +0300

    increase interval request time

--- src/js/Components/globals.js
+++ src/js/Components/globals.js
@@ -1,4 +1,4 @@
 export const apiUrl = 'https://ahj-rxjs-task-1-backend.onrender.com';
-export const intervalTime = 5000;
+export const intervalTime = 60000;
 // export const apiUrl = 'http://localhost:7070';
 
